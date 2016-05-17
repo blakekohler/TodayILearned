8f888b2b8cc773a7f6262ddfabe7f07e970c82ec
bkohlercenteva <blake.kohler@centeva.com>
2016-05-17 13:45:55 -0600

    commit

--- app/main.ts
+++ app/main.ts
@@ -3,10 +3,11 @@ import { AppComponent } from './app.component';
 import {provide} from 'angular2/core';
 import {ROUTER_PROVIDERS} from 'angular2/router';
 import {ELEMENT_PROBE_PROVIDERS} from 'angular2/platform/common_dom';
-
+//import { HTTP_PROVIDERS }    from 'angular/http';
 
 bootstrap(AppComponent, [
         ROUTER_PROVIDERS,
+        //HTTP_PROVIDERS,
         // set a custom default options for the modal.
         ELEMENT_PROBE_PROVIDERS // remove in production
     ])
